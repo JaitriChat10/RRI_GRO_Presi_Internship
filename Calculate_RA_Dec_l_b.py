6f15809f6c8aac1170eaf070c1db55942104e33a
JaitriChat10 <jaitri.lm10@gmail.com>
2026-02-23 21:46:32 +0530

    Code for calculation of RA, DEC, l and b
    Calculation of RA, DEC, Galactic Longitude and Galactic Latitude

---
+++ Calculate_RA_Dec_l_b.py
@@ -0,0 +1,81 @@
+import numpy as np
+from datetime import datetime, timezone
+
+#Function for Julian Day
+def Julian_Day(year, month, day, hour, minute, sec):
+    a = (14 - month) // 12
+    y = year + 4800 - a
+    m = month + 12 * a - 3
+    
+    jdn = day + (153 * m + 2) // 5 + 365 * y + y // 4 - y // 100 + y // 400 - 32045
+    JD = jdn + (hour - 12) / 24 + minute / 1440 + sec / 86400
+    return JD
+
+#Function for Local Sidereal Time
+def LST(east_long, dt):
+    jd = Julian_Day(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    jdt = int(jd + 0.5) - 0.5
+
+    d = jdt - 2451545
+    T = d / 36525
+   
+    gmst = 280.46061837 + 360.98564736629*(jd - 2451545.0) + 0.000387933*(T**2) - (T**3)/38710000           #Greenwich Mean Sidereal Time
+    gmst = gmst % 360
+
+    lmst = (gmst + east_long) 
+    return lmst
+
+#Function for conversion from Horizontal to Equatorial System
+def Horizontal_Equatorial(a, A, lat, lst_deg):
+    a = np.radians(a)           #Altitude
+    A = np.radians(A)           #Azimuth
+    lat = np.radians(lat)       #Latitude
+    lst = np.radians(lst_deg)   #LST
+
+    # Declination
+    sin_dec = np.sin(a) * np.sin(lat) + np.cos(a) * np.cos(lat) * np.cos(A)
+    dec = np.asin(sin_dec)
+
+    # Hour Angle
+    cos_h = (np.sin(a) - np.sin(lat)*np.sin(dec)) / (np.cos(lat)*np.cos(dec))
+    h = np.acos(cos_h)
+
+    # Adjust HA depending on azimuth
+    if np.sin(A) > 0:
+        h = 2*np.pi - h
+
+    # Right Ascension
+    ra = lst - h
+
+    ra = ra % (2*np.pi)
+
+    return np.degrees(ra), np.degrees(dec)
+    
+    
+#Interactive cell for entering required details
+if __name__ == "__main__":
+    print("Enter observation site and antenna orientation details:")
+
+    lat = float(input("Latitude (in degrees, +N): "))
+    lon = float(input("Longitude (in degrees, +E): "))
+    alt = float(input("Altitude (in degrees): "))
+    az = float(input("Azimuth (in degrees, 0=N, 90=E): "))
+
+    date_str = input("Enter observation time (YYYY-MM-DD HH:MM:SS): ")
+    dt = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+    dt = dt.astimezone(timezone.utc)
+
+    # Calculate LST
+    lst = LST(lon, dt) 
+
+    # Convert to RA-Dec
+    ra, dec = Horizontal_Equatorial(alt, az, lat, lst)
+
+    # Convert to Galactic
+    glon, glat = Equatorial_Galactic(ra, dec)
+
+    print("RESULTS:")
+    print(f"Right Ascension (in degree): {ra:.2f}")
+    print(f"Declination (in degree): {dec:.2f}")
+    print(f"Galactic Longitude (in degree): {glon:.2f}")
+    print(f"Galactic Latitude (in degree): {glat:.2f}")
